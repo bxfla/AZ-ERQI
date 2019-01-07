fd91e50fcd700542b2a8f1c0aad08c4c808f082f
huxf <914036951@qq.com>
2019-01-07 17:12:51 +0800

    2018-12-17

--- app/src/main/java/az/com/newazhong/utilsclass/person/activity/MainActivity.java
+++ app/src/main/java/az/com/newazhong/utilsclass/person/activity/MainActivity.java
@@ -7,10 +7,13 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -18,14 +21,12 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
-import az.com.newazhong.Fragment_Adapter;
 import az.com.newazhong.R;
 import az.com.newazhong.SharedPreferencesHelper;
 import az.com.newazhong.utilsclass.base.AlertDialogCallBack;
 import az.com.newazhong.utilsclass.base.BaseActivity;
 import az.com.newazhong.utilsclass.bean.Version;
 import az.com.newazhong.utilsclass.myViews.Header;
-import az.com.newazhong.utilsclass.myViews.NoScrollViewPager;
 import az.com.newazhong.utilsclass.person.fragment.Fragment0;
 import az.com.newazhong.utilsclass.person.fragment.Fragment1;
 import az.com.newazhong.utilsclass.person.fragment.Fragment2;
@@ -36,8 +37,9 @@ import az.com.newazhong.utilsclass.utils.AlertDialogUtil;
 import az.com.newazhong.utilsclass.view.VersionView;
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-public class MainActivity extends BaseActivity implements RadioGroup.OnCheckedChangeListener, VersionView {
+public class MainActivity extends BaseActivity implements VersionView {
 
     @Bind(R.id.header)
     Header header;
@@ -50,12 +52,13 @@ public class MainActivity extends BaseActivity implements RadioGroup.OnCheckedCh
     @Bind(R.id.main_rg)
     RadioGroup mainRg;
     @Bind(R.id.fragment_container)
-    NoScrollViewPager fragmentContainer;
+    FrameLayout fragmentContainer;
     AlertDialogUtil alertDialogUtil;
     VersionPresenter presenter;
     public Fragment fragment01, fragment02, fragment03;
     private static boolean isExit = false;
     List<Fragment> list = new ArrayList<Fragment>();
+    Fragment mContent;
     String tag;
     //推出程序
     Handler mHandler = new Handler() {
@@ -73,7 +76,7 @@ public class MainActivity extends BaseActivity implements RadioGroup.OnCheckedCh
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ButterKnife.bind(this);
-        new SharedPreferencesHelper(this,"fragment_tag").saveData(this,"fragment_tag","1");
+        new SharedPreferencesHelper(this, "fragment_tag").saveData(this, "fragment_tag", "1");
         ActivityCollector.addActivity(this);
         Intent intent = getIntent();
         tag = intent.getStringExtra("tag");
@@ -105,23 +108,23 @@ public class MainActivity extends BaseActivity implements RadioGroup.OnCheckedCh
     }
 
 
-    @Override
-    public void onCheckedChanged(RadioGroup group, int checkedId) {
-        switch (checkedId) {
-            case R.id.rb0:
-                fragmentContainer.setCurrentItem(0, false);
-                new SharedPreferencesHelper(this,"fragment_tag").saveData(this,"fragment_tag","1");
-                break;
-            case R.id.rb1:
-                fragmentContainer.setCurrentItem(1, false);
-                new SharedPreferencesHelper(this,"fragment_tag").saveData(this,"fragment_tag","2");
-                break;
-            case R.id.rb2:
-                fragmentContainer.setCurrentItem(2, false);
-                new SharedPreferencesHelper(this,"fragment_tag").saveData(this,"fragment_tag","3");
-                break;
-        }
-    }
+//    @Override
+//    public void onCheckedChanged(RadioGroup group, int checkedId) {
+//        switch (checkedId) {
+//            case R.id.rb0:
+//                fragmentContainer.setCurrentItem(0, false);
+//                new SharedPreferencesHelper(this,"fragment_tag").saveData(this,"fragment_tag","1");
+//                break;
+//            case R.id.rb1:
+//                fragmentContainer.setCurrentItem(1, false);
+//                new SharedPreferencesHelper(this,"fragment_tag").saveData(this,"fragment_tag","2");
+//                break;
+//            case R.id.rb2:
+//                fragmentContainer.setCurrentItem(2, false);
+//                new SharedPreferencesHelper(this,"fragment_tag").saveData(this,"fragment_tag","3");
+//                break;
+//        }
+//    }
 
     @Override
     protected int provideContentViewId() {
@@ -179,27 +182,67 @@ public class MainActivity extends BaseActivity implements RadioGroup.OnCheckedCh
             fragment01 = new Fragment0();
             fragment02 = new Fragment1();
             fragment03 = new Fragment2();
-            list.add(fragment01);
-            list.add(fragment02);
-            list.add(fragment03);
-            mainRg.setOnCheckedChangeListener(this);
-            fragmentContainer.setAdapter(new Fragment_Adapter(getSupportFragmentManager(), list));
-            if (tag != null && tag.equals("1")) {
-                fragmentContainer.setCurrentItem(1, false);
-                rb1.setChecked(true);
-            } else {
-                fragmentContainer.setCurrentItem(0, false);
-            }
+            startFragmentAdd(fragment01);
+//            list.add(fragment01);
+//            list.add(fragment02);
+//            list.add(fragment03);
+//            mainRg.setOnCheckedChangeListener(this);
+//            fragmentContainer.setAdapter(new Fragment_Adapter(getSupportFragmentManager(), list));
+//            if (tag != null && tag.equals("1")) {
+//                fragmentContainer.setCurrentItem(1, false);
+//                rb1.setChecked(true);
+//            } else {
+//                fragmentContainer.setCurrentItem(0, false);
+//            }
         }
     }
 
     @Override
     public void onTrimMemory(int level) {
         super.onTrimMemory(level);
-        switch (level){
+        switch (level) {
             case TRIM_MEMORY_UI_HIDDEN:
                 finish();
                 break;
+        }
+    }
+
+    @OnClick({R.id.rb0, R.id.rb1, R.id.rb2})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.rb0:
+                startFragmentAdd( fragment01);
+                break;
+            case R.id.rb1:
+                startFragmentAdd( fragment02);
+                break;
+            case R.id.rb2:
+                startFragmentAdd( fragment03);
+                break;
+            default:
+                break;
+        }
+    }
+
+    // fragment的切换
+    private void startFragmentAdd(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager
+                .beginTransaction();
+        if (mContent == null) {
+            fragmentTransaction.add(R.id.fragment_container, fragment).commit();
+            mContent = fragment;
+        }
+        if (mContent != fragment) {
+            // 先判断是否被add过
+            if (!fragment.isAdded()) {
+                // 隐藏当前的fragment，add下一个到Activity中
+                fragmentTransaction.hide(mContent).add(R.id.fragment_container, fragment).commit();
+            } else {
+                // 隐藏当前的fragment，显示下一个
+                fragmentTransaction.hide(mContent).show(fragment).commit();
+            }
+            mContent = fragment;
         }
     }
 }
